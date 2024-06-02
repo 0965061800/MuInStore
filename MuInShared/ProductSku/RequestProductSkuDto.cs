a250bdee2c1635240a2e6e6f1a9121a435269710
Hoàng Luật <96806811+0965061800@users.noreply.github.com>
2024-06-02 22:55:27 +0700

    Add Mapper on Product Sku Dto

---
+++ MuInShared/ProductSku/RequestProductSkuDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MuInShared.ProductSku
+{
+	public class RequestProductSkuDto
+	{
+		[Required]
+		public string Sku { get; set; }
+		[Column(TypeName = "decimal(18,2)")]
+		public decimal UnitPrice { get; set; }
+		public int UnitInStock { get; set; }
+		[Required]
+		public int ColorId { get; set; }
+		[Required]
+		public int ProductId { get; set; }
+		public string skuImage { get; set; } = string.Empty;
+		public string ImageName { get; set; } = string.Empty;
+	}
+}
