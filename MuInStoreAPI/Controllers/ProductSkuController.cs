083233a5a7031cfb57d2620ef25789f5c7a96e1a
Hoàng Luật <96806811+0965061800@users.noreply.github.com>
2024-06-02 22:55:57 +0700

    Add CRUD API on Product Sku

--- MuInStoreAPI/Controllers/ProductSkuController.cs
+++ MuInStoreAPI/Controllers/ProductSkuController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MuInShared.ProductSku;
 using MuInStoreAPI.Mappers;
 using MuInStoreAPI.Models;
 using MuInStoreAPI.UnitOfWork;
@@ -26,6 +27,65 @@ namespace MuInStoreAPI.Controllers
 			}
 			var productSkuDto = productSku.ToProductSkuFullDto();
 			return Ok(productSkuDto);
+		}
+		[HttpPost]
+		public async Task<ActionResult> CreateProductSku([FromBody] RequestProductSkuDto requestproductSkudto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+			ProductSku newProductSku = requestproductSkudto.ToProductSku();
+			try
+			{
+				await _uow.ProductSkuRepository.Create(newProductSku);
+				await _uow.Save();
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPut("{id:int}")]
+		public async Task<IActionResult> UpdateSku([FromBody] RequestProductSkuDto requestProductSkuDto, int id)
+		{
+			var productSkuDto = await _uow.ProductSkuRepository.GetById(id);
+			if (productSkuDto == null)
+			{
+				return NotFound("There is no productSku in your database");
+			}
+			productSkuDto = requestProductSkuDto.ToUpdateProductSku(productSkuDto);
+			try
+			{
+				await _uow.ProductSkuRepository.Update(id, productSkuDto);
+				await _uow.Save();
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+		[HttpDelete("{id:int}")]
+		public async Task<IActionResult> DeleteSku(int id)
+		{
+			try
+			{
+				var productSku = await _uow.ProductSkuRepository.GetById(id);
+				if (productSku == null)
+				{
+					return NotFound();
+				}
+				await _uow.ProductSkuRepository.Delete(id);
+				await _uow.Save();
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
 		}
 	}
 }
