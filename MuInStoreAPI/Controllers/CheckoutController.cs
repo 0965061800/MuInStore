5cd0ec4c57d69d160a88c9d6fff6855673245c20
Hoàng Luật <96806811+0965061800@users.noreply.github.com>
2024-06-04 12:59:50 +0700

    Update Register View

--- MuInStoreAPI/Controllers/CheckoutController.cs
+++ MuInStoreAPI/Controllers/CheckoutController.cs
@@ -8,80 +8,78 @@ using MuInStoreAPI.UnitOfWork;
 
 namespace MuInStoreAPI.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class CheckoutController : ControllerBase
-	{
-		private readonly IUnitOfWork _uow;
-		private readonly UserManager<AppUser> _userManager;
-		public CheckoutController(IUnitOfWork uow, UserManager<AppUser> userManager)
-		{
-			_uow = uow;
-			_userManager = userManager;
-		}
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly UserManager<AppUser> _userManager;
+        public CheckoutController(IUnitOfWork uow, UserManager<AppUser> userManager)
+        {
+            _uow = uow;
+            _userManager = userManager;
+        }
 
-		[HttpPost]
-		[Authorize]
-		public async Task<IActionResult> Checkout([FromBody] List<CartItemReponse> cart)
-		{
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Checkout([FromBody] List<CartItemReponse> cart)
+        {
+            try
+            {
+                var username = User.GetUserName();
+                var appUser = await _userManager.FindByNameAsync(username);
+                await _uow.BeginTransactionAsync();
+                try
+                {
 
+                    Payment newPayment = new Payment
+                    {
+                        Amount = Convert.ToInt32(cart.Sum(x => x.TotalMoney)),
+                        PayStatusId = 1
+                    };
+                    await _uow.PaymentRepository.Create(newPayment);
+                    await _uow.Save();
 
-			try
-			{
-				var username = User.GetUserName();
-				var appUser = await _userManager.FindByNameAsync(username);
-				await _uow.BeginTransactionAsync();
-				try
-				{
+                    Order newOrder = new Order
+                    {
+                        SumTotal = Convert.ToInt32(cart.Sum(x => x.TotalMoney)),
+                        Address = appUser.Address,
+                        PaymentId = newPayment.PaymentId,
+                        AppUserId = appUser.Id,
+                    };
+                    await _uow.OrderRepository.Create(newOrder);
+                    await _uow.Save();
 
-					Payment newPayment = new Payment
-					{
-						Amount = Convert.ToInt32(cart.Sum(x => x.TotalMoney)),
-						PayStatusId = 1
-					};
-					await _uow.PaymentRepository.Create(newPayment);
-					await _uow.Save();
+                    foreach (var item in cart)
+                    {
+                        OrderDetail orderDetail = new OrderDetail
+                        {
+                            OrderId = newOrder.OrderId,
+                            ProductSkuId = item.ProductSkuId,
+                            Quantity = item.Amount,
+                            Total = (decimal)item.TotalMoney,
+                            ColorName = item.ColorName ?? "",
+                            ProductName = item.ProductName ?? "",
+                            ProductId = item.ProductId,
+                            UnitPrice = item.UnitPrice,
+                        };
+                        await _uow.OrderDetailRepository.Create(orderDetail);
+                    }
+                    await _uow.Save();
+                    await _uow.CommitAsync();
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    await _uow.RollbackAsync();
+                    return StatusCode(500, ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
 
-					Order newOrder = new Order
-					{
-						SumTotal = Convert.ToInt32(cart.Sum(x => x.TotalMoney)),
-						Address = appUser.Address,
-						PaymentId = newPayment.PaymentId,
-						AppUserId = appUser.Id,
-					};
-					await _uow.OrderRepository.Create(newOrder);
-					await _uow.Save();
-
-					foreach (var item in cart)
-					{
-						OrderDetail orderDetail = new OrderDetail
-						{
-							OrderId = newOrder.OrderId,
-							ProductSkuId = item.ProductSkuId,
-							Quantity = item.Amount,
-							Total = (decimal)item.TotalMoney,
-							ColorName = item.ColorName ?? "",
-							ProductName = item.ProductName ?? "",
-							ProductId = item.ProductId,
-							UnitPrice = item.UnitPrice,
-						};
-						await _uow.OrderDetailRepository.Create(orderDetail);
-					}
-					await _uow.Save();
-					await _uow.CommitAsync();
-					return Ok();
-				}
-				catch (Exception ex)
-				{
-					await _uow.RollbackAsync();
-					return StatusCode(500, ex.Message);
-				}
-			}
-			catch (Exception ex)
-			{
-				return StatusCode(500, ex.Message);
-			}
-
-		}
-	}
+        }
+    }
 }
