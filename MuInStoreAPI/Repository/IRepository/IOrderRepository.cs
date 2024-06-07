0273fae935ae55cbcb3ddbcc9aff3aaacd6d820b
Hoàng Luật <96806811+0965061800@users.noreply.github.com>
2024-06-07 17:13:42 +0700

    Change type of return GetOrderByUserName

--- MuInStoreAPI/Repository/IRepository/IOrderRepository.cs
+++ MuInStoreAPI/Repository/IRepository/IOrderRepository.cs
@@ -3,10 +3,10 @@ using System.Linq.Expressions;
 
 namespace MuInStoreAPI.Repository.IRepository
 {
-	public interface IOrderRepository : IGenericRepository<Order>
-	{
-		Task<List<Order?>> GetOrderByUserName(string userName);
-		Task<List<Order>?> GetAllOrderAsync(Expression<Func<Order, bool>>? filter = null, Func<IQueryable<Order>, IOrderedQueryable<Order>> orderBy = null);
-		Task<Order?> GetOrderById(int id);
-	}
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+        Task<List<Order>?> GetOrderByUserName(string userName);
+        Task<List<Order>?> GetAllOrderAsync(Expression<Func<Order, bool>>? filter = null, Func<IQueryable<Order>, IOrderedQueryable<Order>> orderBy = null);
+        Task<Order?> GetOrderById(int id);
+    }
 }
